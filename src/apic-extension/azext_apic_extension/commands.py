398b1d47366010d3b2800258162f0b25e29bd79b
Chaoyi Yuan <chyuan@microsoft.com>
2024-04-16 13:03:50 +0800

    fix: register commands with new name and related patches

--- src/apic-extension/azext_apic_extension/commands.py
+++ src/apic-extension/azext_apic_extension/commands.py
@@ -20,9 +20,9 @@ def load_custom_commands(self, _):  # pylint: disable=unused-argument
     with self.command_group('apic api definition') as g:
         self.command_table['apic api definition import-specification'] = ImportSpecificationExtension(loader=self)
         self.command_table['apic api definition export-specification'] = ExportSpecificationExtension(loader=self)
-    with self.command_group('apic metadata-schema') as g:
-        self.command_table['apic metadata-schema create'] = CreateMetadataSchemaExtension(loader=self)
-        self.command_table['apic metadata-schema update'] = UpdateMetadataSchemaExtension(loader=self)
-        self.command_table['apic metadata-schema export-metadata-schema'] = ExportMetadataSchemaExtension(loader=self)
+    with self.command_group('apic metadata') as g:
+        self.command_table['apic metadata create'] = CreateMetadataSchemaExtension(loader=self)
+        self.command_table['apic metadata update'] = UpdateMetadataSchemaExtension(loader=self)
+        self.command_table['apic metadata export'] = ExportMetadataSchemaExtension(loader=self)
     with self.command_group('apic api') as g:
         g.custom_command("register", "register_apic")
