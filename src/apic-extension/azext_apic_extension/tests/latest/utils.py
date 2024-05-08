9478a62c0cefb96075bbbf49337c6ec6f1065645
Chaoyi Yuan <chyuan@microsoft.com>
2024-05-08 14:35:55 +0800

    test: test cases for deployment commands

--- src/apic-extension/azext_apic_extension/tests/latest/utils.py
+++ src/apic-extension/azext_apic_extension/tests/latest/utils.py
@@ -226,7 +226,7 @@ class ApicVersionPreparer(NoTrafficRecordingPreparer, SingleValueReplacer):
         
 class ApicDefinitionPreparer(NoTrafficRecordingPreparer, SingleValueReplacer):
     def __init__(self, name_prefix='clitest', length=24,
-                 parameter_name='version_id', resource_group_parameter_name='resource_group',
+                 parameter_name='definition_id', resource_group_parameter_name='resource_group',
                  apic_service_parameter_name='service_name', apic_api_parameter_name='api_id',
                  apic_version_parameter_name='version_id', key='d'):
         super(ApicDefinitionPreparer, self).__init__(name_prefix, length)
@@ -287,3 +287,88 @@ class ApicDefinitionPreparer(NoTrafficRecordingPreparer, SingleValueReplacer):
             template = 'To create an API Center Definition an API Center Version is required. Please add ' \
                        'decorator @{} in front of this preparer.'
             raise CliTestError(template.format(ApicVersionPreparer.__name__))
+        
+class ApicDeploymentPreparer(NoTrafficRecordingPreparer, SingleValueReplacer):
+    def __init__(self, name_prefix='clitest', length=24,
+                 parameter_name='deployment_id', resource_group_parameter_name='resource_group',
+                 apic_service_parameter_name='service_name', apic_api_parameter_name='api_id',
+                 apic_version_parameter_name='version_id', apic_definition_parameter_name='definition_id',
+                 apic_environment_parameter_name='environment_id', key='dep'):
+        super(ApicDeploymentPreparer, self).__init__(name_prefix, length)
+        self.cli_ctx = get_dummy_cli()
+        self.resource_group_parameter_name = resource_group_parameter_name
+        self.apic_service_parameter_name = apic_service_parameter_name
+        self.parameter_name = parameter_name
+        self.apic_api_parameter_name = apic_api_parameter_name
+        self.apic_version_parameter_name = apic_version_parameter_name
+        self.apic_definition_parameter_name = apic_definition_parameter_name
+        self.apic_environment_parameter_name = apic_environment_parameter_name
+        self.key = key
+
+    def create_resource(self, name, **kwargs):
+        group = self._get_resource_group(**kwargs)
+        service = self._get_apic_service(**kwargs)
+        api = self._get_apic_api(**kwargs)
+        version = self._get_apic_version(**kwargs)
+        definition = self._get_apic_definition(**kwargs)
+        environment = self._get_apic_environment(**kwargs)
+
+        template = 'az apic api deployment create -g {} -s {} --api-id {} --definition-id /workspaces/default/apis/{}/versions/{}/definitions/{} --environment-id /workspaces/default/environments/{} --deployment-id {} --title "test deployment" --server \'{{"runtimeUri":["https://example.com"]}}\''
+        cmd = template.format(group, service, api, api, version, definition, environment, name)
+        print(cmd)
+        self.live_only_execute(self.cli_ctx, cmd)
+
+        self.test_class_instance.kwargs[self.key] = name
+        return {self.parameter_name: name}
+
+    def remove_resource(self, name, **kwargs):
+        # ResourceGroupPreparer will delete everything
+        pass
+
+    def _get_resource_group(self, **kwargs):
+        try:
+            return kwargs.get(self.resource_group_parameter_name)
+        except KeyError:
+            template = 'To create an API Center Deployment a resource group is required. Please add ' \
+                       'decorator @{} in front of this preparer.'
+            raise CliTestError(template.format(ResourceGroupPreparer.__name__))
+
+    def _get_apic_service(self, **kwargs):
+        try:
+            return kwargs.get(self.apic_service_parameter_name)
+        except KeyError:
+            template = 'To create an API Center Deployment an API Center service is required. Please add ' \
+                       'decorator @{} in front of this preparer.'
+            raise CliTestError(template.format(ApicServicePreparer.__name__))
+
+    def _get_apic_api(self, **kwargs):
+        try:
+            return kwargs.get(self.apic_api_parameter_name)
+        except KeyError:
+            template = 'To create an API Center Deployment an API Center API is required. Please add ' \
+                       'decorator @{} in front of this preparer.'
+            raise CliTestError(template.format(ApicApiPreparer.__name__))
+        
+    def _get_apic_version(self, **kwargs):
+        try:
+            return kwargs.get(self.apic_version_parameter_name)
+        except KeyError:
+            template = 'To create an API Center Deployment an API Center Version is required. Please add ' \
+                       'decorator @{} in front of this preparer.'
+            raise CliTestError(template.format(ApicVersionPreparer.__name__))
+        
+    def _get_apic_definition(self, **kwargs):
+        try:
+            return kwargs.get(self.apic_definition_parameter_name)
+        except KeyError:
+            template = 'To create an API Center Deployment an API Center Definition is required. Please add ' \
+                       'decorator @{} in front of this preparer.'
+            raise CliTestError(template.format(ApicDefinitionPreparer.__name__))
+        
+    def _get_apic_environment(self, **kwargs):
+        try:
+            return kwargs.get(self.apic_environment_parameter_name)
+        except KeyError:
+            template = 'To create an API Center Deployment an API Center Environment is required. Please add ' \
+                       'decorator @{} in front of this preparer.'
+            raise CliTestError(template.format(ApicEnvironmentPreparer.__name__))
