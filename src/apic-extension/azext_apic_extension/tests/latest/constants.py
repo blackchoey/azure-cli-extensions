aa7a7bf28f261f25454074b601fa7eacabf215a6
frankqianms <frankqian@microsoft.com>
2024-10-23 17:00:59 +0800

    .

--- src/apic-extension/azext_apic_extension/tests/latest/constants.py
+++ src/apic-extension/azext_apic_extension/tests/latest/constants.py
@@ -3,5 +3,8 @@
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
 
+import os
+
 TEST_REGION = "eastus"
-USERASSIGNED_IDENTITY = "{/subscriptions/976c6e22-5aa9-47a4-a6db-bc1dcfebf792/resourceGroups/rg-apim-reader-MI/providers/Microsoft.ManagedIdentity/userAssignedIdentities/apim-reader-MI}"
+USERASSIGNED_IDENTITY = os.getenv('USERASSIGNED_IDENTITY')
+USERASSIGNED_IDENTITY = USERASSIGNED_IDENTITY if USERASSIGNED_IDENTITY else "{/subscriptions/976c6e22-5aa9-47a4-a6db-bc1dcfebf792/resourceGroups/rg-apim-reader-MI/providers/Microsoft.ManagedIdentity/userAssignedIdentities/apim-reader-MI}"
