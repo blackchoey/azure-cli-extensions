6cdd6bd368049cbd3de946d623fa445382e1d44c
frankqianms <frankqian@microsoft.com>
2024-12-25 17:19:20 +0800

    fix: remove key value

--- src/apic-extension/azext_apic_extension/tests/latest/constants.py
+++ src/apic-extension/azext_apic_extension/tests/latest/constants.py
@@ -7,8 +7,7 @@ import os
 
 TEST_REGION = "eastus"
 # to set USERASSIGNED_IDENTITY, refer to https://learn.microsoft.com/en-us/azure/api-center/import-api-management-apis?tabs=portal#option-2-import-apis-directly-from-your-api-management-instance
-# USERASSIGNED_IDENTITY = os.getenv('USERASSIGNED_IDENTITY')
-USERASSIGNED_IDENTITY = "/subscriptions/b72ba90c-17fc-4d42-a890-2de38220b1a9/resourceGroups/rg-apic-extension-cli-test/providers/Microsoft.ManagedIdentity/userAssignedIdentities/apic-extension-cli-test"
+USERASSIGNED_IDENTITY = os.getenv('USERASSIGNED_IDENTITY')
 # aws credentials KeyVault references
 ACCESS_KEY_LINK = "https://kv-canary-franktest.vault.azure.net/secrets/AccessKey"
 SECRET_ACCESS_KEY_LINK = "https://kv-canary-franktest.vault.azure.net/secrets/SecretAccessKey"
