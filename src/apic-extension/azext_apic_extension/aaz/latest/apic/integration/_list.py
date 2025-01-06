b32eca6c7cb5696f3c939a831b53074cbf5080c8
frankqianms <frankqian@microsoft.com>
2025-01-06 14:18:53 +0800

    fix: integration list

--- src/apic-extension/azext_apic_extension/aaz/latest/apic/integration/_list.py
+++ src/apic-extension/azext_apic_extension/aaz/latest/apic/integration/_list.py
@@ -48,7 +48,7 @@ class List(AAZCommand):
             required=True,
         )
         _args_schema.service_name = AAZStrArg(
-            options=["-n", "--service-name"],
+            options=["-s", "--name", "--service", "--service-name"],
             help="The name of Azure API Center service.",
             required=True,
             fmt=AAZStrArgFormat(
